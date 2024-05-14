99d98aa97174dcc06181636a641882de7958e893
fabriciofabras <fabriciofabras@gmail.com>
2024-05-14 11:43:23 -0600

    Organización menú

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -29,23 +29,39 @@ export const NavBar = ({ onOpcionSeleccionada }) => {
           </a>
         </a>
         <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700	flex flex-wrap items-center text-base justify-center">
-          <a href="#projects" className="mr-5 hover:text-white">
+        {/*   <a href="https://atalait.sharepoint.com/:f:/r/sites/panelsari2p1/Otra%20informacin/04%20Monitoreo%20y%20control/Inventarios?csf=1&web=1&e=Db3NPq" target="_blank" className="mr-5 hover:text-white">
             Repositorio de Información
-          </a>
-          <a onClick={() => handleClick('monitoreo')} href="#skills" className="mr-5 hover:text-white">
+          </a> */}
+          <ul class="menu">
+          <li class="menu-item dropdown">
+                <a href="#">Repositorio</a>
+                <ul class="dropdown-menu">
+                    <li><a href="https://atalait.sharepoint.com/:f:/r/sites/panelsari2p1/Otra%20informacin/04%20Monitoreo%20y%20control/Inventarios?csf=1&web=1&e=Db3NPq" target="_blank">Repositorio de Información</a></li>
+                    <li><a href="https://atalait.sharepoint.com/sites/panelsari2p1/Monitoreo%20y%20Mesa%20de%20Ayuda/Forms/AllItems.aspx?viewid=7eb3a40d%2Ddde6%2D457f%2Dbb30%2D846a18447a53" target="_blank">Monitoreo y Mesa de Ayuda</a></li>
+                    <li><a href="https://atalait.sharepoint.com/sites/panelsari2p1/Otra%20informacin/Forms/AllItems.aspx?csf=1&web=1&e=Db3NPq&cid=4209bcc8%2D81e9%2D4705%2Db94e%2D6d376eb3a3c9&FolderCTID=0x012000F022AB04352F8E49A32528F5807B4D24&id=%2Fsites%2Fpanelsari2p1%2FOtra%20informacin%2F04%20Monitoreo%20y%20control%2FInventarios" target="_blank">Inventarios</a></li>
+                    <li><a href="https://atalait.sharepoint.com/sites/panelsari2p1/Otra%20informacin/Forms/AllItems.aspx?viewid=93c323ea%2D0274%2D4b99%2Db68c%2D5d426003803f" target="_blank">Otra información</a></li>
+                </ul>
+          </li>
+          </ul>
+
+          <a  href="#monitoreo" onClick={() => handleClick('monitoreo')} className="mr-5 hover:text-white">
             Monitoreo
           </a>
-          <a href="#skills" className="mr-5 hover:text-white">
-             Mesa de Ayuda
-          </a>
-          <a href="#skills" className="mr-5 hover:text-white">
+         
+         {/*  <a href="https://atalait.sharepoint.com/sites/panelsari2p1/Otra%20informacin/Forms/AllItems.aspx?viewid=93c323ea%2D0274%2D4b99%2Db68c%2D5d426003803f" target="_blank" className="mr-5 hover:text-white">
             Otra Información
-          </a>
-          <a onClick={() => handleClick('heatmap')} href="#skills" className="mr-5 hover:text-white">
+          </a> */}
+          <a href="#heatmap" onClick={() => handleClick('heatmap')} className="mr-5 hover:text-white">
             HeatMap
           </a>
-          <a onClick={() => handleClick('inventarios')} href="#skills" className="mr-5 hover:text-white">
-            Inventarios
+          <a href="#controldeinventario" onClick={() => handleClick('inventarios')} className="mr-5 hover:text-white">
+            Control de Inventario
+          </a>
+          <a href="https://msi.sat.gob.mx/" target="_blank" className="mr-5 hover:text-white">
+             Mesa de Servicio
+          </a>
+          <a href="https://atalait.service-now.com/navpage.do" target="_blank" className="mr-5 hover:text-white">
+             Service Now
           </a>
           {/* <a href="#testimonials" className="mr-5 hover:text-white">
             Testimonios
